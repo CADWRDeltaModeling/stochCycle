404585828591aea7a4125d4cf0dd9d24c6292005
Eli Ateljevich <Eli.Ateljevich@water.ca.gov>
2022-12-17 23:17:37 -0800

    Moved data to data and data-raw

---
+++ R/data.R
@@ -0,0 +1,23 @@
+
+
+#' Tidal frequency list
+#'
+#' Includes selected constituents and species
+#'
+#'
+#' @format ## `tide_freq_radhr`
+#' A vector with labels giving the name of the frequency and value frequency in radians/hr
+"tide_freq_radhr"
+
+
+#' Sacramento River at Freeport discharge
+#'
+#' Data come from the USGS station at Sacramento River in California at Freeport,
+#' station 11447650. The data begin on January 1, 2014 and have a 15min time step
+#' and a unit of cubic feet per second. The data for Ateljevich and Wang (2023)
+#' starts at index 5000.
+#'
+#'
+#' @format ## `freeport_discharge`
+#' A vector of discharge
+"freeport_discharge"
---
+++ data-raw/freeport_discharge.R
@@ -0,0 +1,4 @@
+
+
+ts <- read.table("https://waterservices.usgs.gov/nwis/iv/?sites=11447650&parameterCd=72137&startDT=2014-01-01T00:00&endDT=2014-10-01T00:00&siteStatus=all&format=rdb",
+                 header=TRUE,)
